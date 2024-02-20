1ec278f671ae373eef13a048bfd37904b0060a8b
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 15:09:13 +0300

    Methods with parameter.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -11,12 +11,20 @@ namespace Methods
         static void Main(string[] args)
         {
             Add();
+            var result = Add2(20, 30);
+            Console.WriteLine(result);
+
             Console.ReadLine();
         }
 
         static void Add()
         {
             Console.WriteLine("Added!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            return number1 + number2;
         }
     }
 }
