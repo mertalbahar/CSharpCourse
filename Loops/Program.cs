2aa11dc0fda5a7cb6712b07c58d936636733e18c
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 17:31:59 +0300

    For loop.

---
+++ Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
