bb9a53137cc78f2eea23c584ac715e56939a99af
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 17:00:22 +0300

    Arrays.

---
+++ Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            string[] students2 =
+            {
+                "Engin",
+                "Derin",
+                "Salih"
+            };
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
