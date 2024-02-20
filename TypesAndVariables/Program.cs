d8871ca4deabfb864b6ff1886dd915a126d6653d
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 10:36:48 +0300

    Integer value types.

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -10,7 +10,10 @@ namespace TypesAndVariables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+            int number1 = -2147483648; // min value for int
+            int number2 = 2147483647; // max value for int
+            Console.WriteLine("Number1 is {0} and Number2 is {1}", number1, number2);
             Console.ReadLine();
         }
     }
