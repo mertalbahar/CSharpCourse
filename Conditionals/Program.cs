8c9688b7b44136c80d178c523c1e73cdd1207697
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 14:58:14 +0300

    if condition demo.

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -41,6 +41,19 @@ namespace Conditionals
                     break;
             }
 
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less then 0 or greater then 200");
+            }
+
             Console.ReadLine();
         }
     }
