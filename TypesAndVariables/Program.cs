8c5a9512ce5679bf419581c9df773781a49ea3e4
MertalBahar <mertalbahar@windowslive.com>
2024-02-20 10:50:10 +0300

    Short value type.

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,13 +11,18 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World");
+            short number5 = -32768; // min value for short, 16 bit
+            short number6 = 32767; // max value for short, 16 bit
+
+            Console.WriteLine("Short(16) min {0} and Short max {1}", number5, number6);
+
             int number1 = -2147483648; // min value for int, 32 bit
             int number2 = 2147483647; // max value for int, 32 bit
-            Console.WriteLine("Number1 is {0} and Number2 is {1}", number1, number2);
+            Console.WriteLine("int(32) min {0} and int max {1}", number1, number2);
 
             long number3 = -9223372036854775808; // min value for long, 64 bit
             long number4 = 9223372036854775807; // max value for long, 64 bit
-            Console.WriteLine("Number3 is {0} and Number4 is {1}", number3, number4);
+            Console.WriteLine("long(64) min {0} and long max {1}", number3, number4);
 
             Console.ReadLine();
         }
