6f9401a73362f5275f5313c4b56932470cc4f31c
MertalBahar <mertalbahar@windowslive.com>
2024-02-22 13:42:47 +0300

    Classes intro.

---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productrManager = new ProductManager();
+            productrManager.Add();
+            productrManager.Update();
+
+            Console.ReadLine();
+        }
+    }
+}
