bbb83f1a3d16682efafa89127705430f351c7843
MertalBahar <mertalbahar@windowslive.com>
2024-02-22 12:56:54 +0300

    String method.

---
+++ Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+            // Console.WriteLine(city[0]);
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "İstanbul";
+            string result = city + city2;
+            string result2 = String.Format("{0} {1}", city, city2);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine("{0} {1}", city, city2);
+
+            Console.ReadLine();
+        }
+    }
+}
