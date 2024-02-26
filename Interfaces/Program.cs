5d751d3d4939bb1de7d73dbe58183b3dc591fb74
MertalBahar <mertalbahar@windowslive.com>
2024-02-26 09:56:56 +0300

    Interfaces usage.

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,6 +10,37 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+            };
+
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Department = "Computer Sciences"
+            };
+
+            Worker worker = new Worker
+            {
+                Id = 3,
+                FirstName = "Salih",
+                LastName = "Demiroğ",
+                Department = "Production"
+            };
+
+            manager.Add(customer);
+            manager.Add(student);
+            manager.Add(worker);
+
+            Console.ReadLine();
         }
     }
 
@@ -34,5 +65,21 @@ namespace Interfaces
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Department { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
